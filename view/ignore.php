b181eaeb44d965ee022d2c560334e06360e59ba0
nabeel <mohamednabeel0170@gmail.com>
2024-01-07 02:52:17 +0530

    feat:chef/view waiting orders

--- view/ignore.php
+++ view/ignore.php
@@ -1,121 +1,143 @@
 var requiredqty = document.createElement('input');
-        requiredqty.setAttribute('type', 'number');
-        requiredqty.setAttribute('placeholder', 'required quantity');
-        requiredqty.setAttribute('name', 'required_quantity[]');
-        requiredqty.setAttribute('id', 'required_quantity');
-        requiredqty.required = true;
+requiredqty.setAttribute('type', 'number');
+requiredqty.setAttribute('placeholder', 'required quantity');
+requiredqty.setAttribute('name', 'required_quantity[]');
+requiredqty.setAttribute('id', 'required_quantity');
+requiredqty.required = true;
 
 
-        var factor = document.createElement('select');
-        factor.setAttribute('name', 'factor[]');
+var factor = document.createElement('select');
+factor.setAttribute('name', 'factor[]');
 
-        var g = document.createElement('option');
-                g.value = '1';
-                g.text = 'g';
-                factor.appendChild(g);
-                var kg = document.createElement('option');
-                kg.value = '2';
-                kg.text = 'kg';
-                factor.appendChild(kg);
-                var c = document.createElement('option');
-                c.value = '3';
-                c.text = 'c';
-                factor.appendChild(c);
-                var tbsp = document.createElement('option');
-                tbsp.value = '4';
-                tbsp.text = 'tbsp';
-                factor.appendChild(tbsp);
-                var tsp = document.createElement('option');
-                tsp.value = '5';
-                tsp.text = 'tsp';
-                factor.appendChild(tsp);
-                var oz = document.createElement('option');
-                oz.value = '6';
-                oz.text = 'oz';
-                factor.appendChild(oz);
-                var lb = document.createElement('option');
-                lb.value = '7';
-                lb.text = 'lb';
-                factor.appendChild(lb);
-                var ml = document.createElement('option');
-                ml.value = '8';
-                ml.text = 'ml';
-                factor.appendChild(ml);
-                var l = document.createElement('option');
-                l.value = '9';
-                l.text = 'l';
-                factor.appendChild(l);
-    
-
+var g = document.createElement('option');
+g.value = '1';
+g.text = 'g';
+factor.appendChild(g);
+var kg = document.createElement('option');
+kg.value = '2';
+kg.text = 'kg';
+factor.appendChild(kg);
+var c = document.createElement('option');
+c.value = '3';
+c.text = 'c';
+factor.appendChild(c);
+var tbsp = document.createElement('option');
+tbsp.value = '4';
+tbsp.text = 'tbsp';
+factor.appendChild(tbsp);
+var tsp = document.createElement('option');
+tsp.value = '5';
+tsp.text = 'tsp';
+factor.appendChild(tsp);
+var oz = document.createElement('option');
+oz.value = '6';
+oz.text = 'oz';
+factor.appendChild(oz);
+var lb = document.createElement('option');
+lb.value = '7';
+lb.text = 'lb';
+factor.appendChild(lb);
+var ml = document.createElement('option');
+ml.value = '8';
+ml.text = 'ml';
+factor.appendChild(ml);
+var l = document.createElement('option');
+l.value = '9';
+l.text = 'l';
+factor.appendChild(l);
 
 
-                BEGIN
-    
-    IF NEW.factor = '8' THEN
-        SET NEW.`qty_required(ml)` = NEW.`qty_required(g)`;
-    END IF;
-    IF NEW.factor = '9' THEN
-        SET NEW.`qty_required(ml)` = NEW.`qty_required(g)` / 1000;
-    END IF;
+
+
+BEGIN
+
+IF NEW.factor = '8' THEN
+SET NEW.`qty_required(ml)` = NEW.`qty_required(g)`;
+END IF;
+IF NEW.factor = '9' THEN
+SET NEW.`qty_required(ml)` = NEW.`qty_required(g)` / 1000;
+END IF;
 END
 
 BEGIN
 IF NEW.factor = '1' THEN
-        SET NEW.`qty_required(g)` = NEW.`qty_required(g)`;
-        END IF;
+SET NEW.`qty_required(g)` = NEW.`qty_required(g)`;
+END IF;
 
-    IF NEW.factor = '2' THEN
-        SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 1000;
-        END IF;
-        IF NEW.factor = '3' THEN
-        SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 250;
-    END IF;
-    IF NEW.factor = '4' THEN
-        SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 14.175;
-    END IF;
-    IF NEW.factor = '5' THEN
-        SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 5.69;
-    END IF;
-    IF NEW.factor = '6' THEN
-        SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 28.3495;
-    END IF;
-    IF NEW.factor = '7' THEN
-        SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 453.592;
-    END IF;
-    IF NEW.factor = '8' THEN
-        SET NEW.`qty_required(ml)` = NEW.`qty_required(g)`;
-    END IF;
-    IF NEW.factor = '9' THEN
-        SET NEW.`qty_required(ml)` = NEW.`qty_required(g)` * 1000 ;
-    END IF;
-   
+IF NEW.factor = '2' THEN
+SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 1000;
+END IF;
+IF NEW.factor = '3' THEN
+SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 250;
+END IF;
+IF NEW.factor = '4' THEN
+SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 14.175;
+END IF;
+IF NEW.factor = '5' THEN
+SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 5.69;
+END IF;
+IF NEW.factor = '6' THEN
+SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 28.3495;
+END IF;
+IF NEW.factor = '7' THEN
+SET NEW.`qty_required(g)` = NEW.`qty_required(g)` * 453.592;
+END IF;
+IF NEW.factor = '8' THEN
+SET NEW.`qty_required(ml)` = NEW.`qty_required(g)`;
+END IF;
+IF NEW.factor = '9' THEN
+SET NEW.`qty_required(ml)` = NEW.`qty_required(g)` * 1000 ;
+END IF;
+
 END
 
 
 function increaseDecreasefooditemqty(button) {
-    var card = button.parentElement.parentElement.parentElement; // Go up three levels to the main container
-    var qtyInput = card.querySelector('.qty-box');
-    var qty = parseInt(qtyInput.value) || 0;
-  
-    if (button.classList.contains('add-btn')) {
-       qty += 1;
-    } else if (button.classList.contains('subtract-btn')) {
-       qty = Math.max(0, qty - 1);
-    }
- 
-    qtyInput.value = qty;
- }
-  function increaseDecreasefooditemqtymanually(value) {
-    var inputvalue = value;
-    console.log(inputvalue);
-    var card = document.activeElement.closest('.card'); // Go up three levels to the main container
-    console.log(card);
-   var clonedcard = card.cloneNode(true);
-   var itemsContainer = document.getElementById('fooditems');
+var card = button.parentElement.parentElement.parentElement; // Go up three levels to the main container
+var qtyInput = card.querySelector('.qty-box');
+var qty = parseInt(qtyInput.value) || 0;
 
-    
- }
- 
+if (button.classList.contains('add-btn')) {
+qty += 1;
+} else if (button.classList.contains('subtract-btn')) {
+qty = Math.max(0, qty - 1);
+}
+
+qtyInput.value = qty;
+}
+function increaseDecreasefooditemqtymanually(value) {
+var inputvalue = value;
+console.log(inputvalue);
+var card = document.activeElement.closest('.card'); // Go up three levels to the main container
+console.log(card);
+var clonedcard = card.cloneNode(true);
+var itemsContainer = document.getElementById('fooditems');
+
+
+}
+
 function addFooditemtoCart(){
 
 }
+
+
+var Ordercard = $(
+' <div class="card col-md-2">'+
+    '<input type="hidden" id="order-id" value="' + orderId + '">'+
+    '<div class="row">
+        <h5>'+'Order Id :'+orderId+'</h5>
+    </div>'+
+    ' <div class="row fooditemDetails">'+
+        '</div>
+</div>'
+);
+var fooditemsDetailsRow = $('.fooditemDetails');
+ordersRow.append(Ordercard);
+
+// for (var i = 0 ; i < fooditemRowArray.length; i++){ // fooditemRow=fooditemRowArray[i];
+    fooditemsDetailsRow.append(fooditemRowArray); // } console.log(); console.log("orders",order); <div class="row">
+    <div class="col"><button type="button" class="btn btn-primary" onclick="">Accept</button>
+    </div>
+    <div class="col"> <button type="button" class="btn btn-success" id=""
+            onclick="">Ready</button></div>
+    </div>
--- view/module/chef/orders/orders.js
+++ view/module/chef/orders/orders.js
@@ -1,13 +1,67 @@
 $(document).ready(function () {
-    displayAllOrders();
+  getAllOrders();
+});
+function getAllOrders() {
+  $.ajax({
+    type: "GET",
+    url: "../../../../controller/order_controller.php?status=get-processing-orders",
+    dataType: "json",
+    success: function (response) {
+      displayAllOrders(response);
+      
+    },
   });
-function displayAllOrders(){
-    $.ajax({
-        type: "GET",
-        url: "../../../../controller/order_controller.php?status=get-placed-orders",
-        dataType: "JSON",
-        success: function (response) {
-            console.log(response);
-        }
-    });
 }
+
+function displayAllOrders(response) {
+    var orderDiv = $('.orders');
+    orderDiv.empty();
+    for (var orders in response){
+        count = 0;
+        var order = response[orders];
+        var fooditemRow = [];
+ for (var i = 0;i < order.length;i++){
+count++;
+var fooditemName = order[i].item_name;
+var orderId = order[i].order_id;
+var quantity = order[i].quantity;
+var fooditemrow = $('<div class="row"><div class="col"><p>#'+count+'</p></div><div class="col"><p>'+fooditemName+'</p></div>'+
+'<div class="col"><p>x'+quantity+'</p></div>'+
+'</div>'
+);
+fooditemRow.push(fooditemrow);
+console.log("here");
+
+ }
+ var Ordercard = $(
+    ' <div class="card " style="width: 15rem; margin: 2px;">'+
+    '<div class="row"><h5 class="card-title">'+'Order Id :'+orderId+'</h5></div>'+
+    ' <div class="row fooditemDetails">'+ 
+    '</div>'+
+   '<div class ="row orderControlButton"><div class="col"><button type="button" class="btn btn-primary" onclick="acceptOrder(orderId)">Accept</button>'+
+   '</div>'+
+  '</div>'+
+   ' </div>'+
+    '</div>'
+    );
+    fooditemRow.forEach(function(){
+        Ordercard.find(".fooditemDetails").append(fooditemRow);
+    });
+    orderDiv.append(Ordercard);
+    console.log(fooditemRow);
+    }
+   
+  }
+
+  function acceptOrder(orderId) {
+$.ajax({
+    type:"POST",
+    url: "../../../../controller/order_controller.php?status=accept-order",
+    data: {order_id: orderId},
+    dataType: "JSON",
+    success: function (response) {
+        console.log(response);
+    }
+});
+  }
+
